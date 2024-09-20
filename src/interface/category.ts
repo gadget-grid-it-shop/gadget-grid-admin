8faa15db64ee7a917ead7e8f036f39fa11009a86
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2024-09-20 15:34:45 +0600

    details category update and delete with rtk

--- src/interface/category.ts
+++ src/interface/category.ts
@@ -1,13 +1,13 @@
 export interface TProductCategory {
-  _id?: string;
+  _id: string;
   name: string;
   fields: string[];
 }
 
 export interface TCategory {
-  name: string,
-  parent_id: string,
-  product_details_categories: TProductCategory[],
-  _id: string,
-  subCategories: TCategory[]
+  name: string;
+  parent_id: string;
+  product_details_categories: TProductCategory[];
+  _id: string;
+  subCategories: TCategory[];
 }
--- src/redux/api/detailsCategory.ts
+++ src/redux/api/detailsCategory.ts
@@ -1,38 +1,51 @@
-import { TProductCategory } from "@/interface/category";
-import { baseApi } from "./baseApi";
-import { tagTypes } from "./tagTypes";
+import {TProductCategory} from "@/interface/category";
+import {baseApi} from "./baseApi";
+import {tagTypes} from "./tagTypes";
 
 const detailsCategoryApi = baseApi.injectEndpoints({
-    endpoints: (build) => ({
-        getDetailsCategories: build.query({
-            query: () => {
-                return {
-                    url: '/product-details-category/get-all',
-                    method: 'GET',
-                }
-            },
-            providesTags: [tagTypes.detailsCategory]
-        }),
-        createDetailsCategory: build.mutation({
-            query: (payload: TProductCategory) => {
-                return {
-                    url: '/product-details-category/create',
-                    method: 'POST',
-                    body: payload,
-                }
-            },
-            invalidatesTags: [tagTypes.detailsCategory]
-        }),
+  endpoints: (build) => ({
+    getDetailsCategories: build.query({
+      query: () => {
+        return {
+          url: "/product-details-category/get-all",
+          method: "GET",
+        };
+      },
+      providesTags: [tagTypes.detailsCategory],
+    }),
+    createDetailsCategory: build.mutation({
+      query: (payload: Pick<TProductCategory, "name" | "fields">) => {
+        return {
+          url: "/product-details-category/create",
+          method: "POST",
+          body: payload,
+        };
+      },
+      invalidatesTags: [tagTypes.detailsCategory],
+    }),
 
-        deleteDetailsCategory: build.mutation({
-            query: ({ payload, id }) => {
-                return {
-                    url: ``
-                }
-            }
-        })
-    })
-})
+    deleteDetailsCategory: build.mutation({
+      query: (id) => {
+        return {
+          url: `${process.env.NEXT_PUBLIC_URL}/product-details-category/delete/${id}`,
+          method: "DELETE",
+        };
+      },
+      invalidatesTags: [tagTypes.detailsCategory],
+    }),
 
+    updateDetailsCategory: build.mutation({
+      query: ({id, payload}: {id: string; payload: Pick<TProductCategory, "name" | "fields">}) => {
+        return {
+          url: `${process.env.NEXT_PUBLIC_URL}/product-details-category/update/${id}`,
+          method: "PATCH",
+          body: payload,
+        };
+      },
+      invalidatesTags: [tagTypes.detailsCategory],
+    }),
+  }),
+});
 
-export const { useGetDetailsCategoriesQuery, useCreateDetailsCategoryMutation } = detailsCategoryApi
+export const {useGetDetailsCategoriesQuery, useCreateDetailsCategoryMutation, useDeleteDetailsCategoryMutation, useUpdateDetailsCategoryMutation} =
+  detailsCategoryApi;
