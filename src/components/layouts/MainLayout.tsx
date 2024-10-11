90fb7f3717b01c05e641c62fdeea009a609f1f56
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2024-10-11 22:02:14 +0600

    user can edit own role

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,58 +1,63 @@
 "use client";
 
-import React, { ReactNode, useEffect, useState } from "react";
+import React, {ReactNode, useEffect, useState} from "react";
 import Navbar from "../shared/Navbar";
 
-import { ToastContainer } from "react-toastify";
+import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useTheme } from "next-themes";
+import {useTheme} from "next-themes";
 import Sidebar from "../shared/Sidebar";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { useRouter } from "next/navigation";
+import {useAppDispatch, useAppSelector} from "@/redux/hooks";
+import {useRouter} from "next/navigation";
 import axiosInstance from "@/lib/axiosInstance";
-import { setUserData } from "@/redux/reducers/auth/authSlice";
+import {setUserData} from "@/redux/reducers/auth/authSlice";
 
-const MainLayout = ({ children }: { children: ReactNode }) => {
-  const { theme } = useTheme();
-  const [hydrated, setHydrated] = useState(false)
-  const { isAuthenticated } = useAppSelector(s => s.auth)
-  const dispatch = useAppDispatch()
-  const router = useRouter()
+const MainLayout = ({children}: {children: ReactNode}) => {
+  const {theme} = useTheme();
+  const [hydrated, setHydrated] = useState(false);
+  const {isAuthenticated} = useAppSelector((s) => s.auth);
+  const dispatch = useAppDispatch();
+  const router = useRouter();
 
   useEffect(() => {
-    setHydrated(true)
-  }, [])
-
+    setHydrated(true);
+  }, []);
 
   useEffect(() => {
     if (isAuthenticated) {
-      axiosInstance.get('/auth/getMyData')
-        .then(res => {
-          console.log(res.data)
-          dispatch(setUserData({ user: res?.data?.data, permissions: res.data?.data?.role?.permissions }))
+      axiosInstance
+        .get("/auth/getMyData")
+        .then((res) => {
+          console.log(res.data);
+          dispatch(
+            setUserData({
+              user: res?.data?.data,
+              permissions: res.data?.data?.role?.permissions,
+            })
+          );
         })
-        .catch(err => {
-          console.log(err)
-        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      router.push("/login");
     }
-    else {
-      router.push('/login')
-    }
-  }, [isAuthenticated, router, dispatch])
-
+  }, [isAuthenticated, router, dispatch]);
 
   return (
     <>
-      {
-        hydrated ? <div className="flex bg-background-foreground">
-          < Sidebar />
+      {hydrated ? (
+        <div className="flex bg-background-foreground">
+          <Sidebar />
           <div className="min-h-screen w-full px-4">
             <Navbar />
             <div className="mt-4 bg-background rounded-md p-5">{children}</div>
           </div>
           <ToastContainer theme={theme} />
-        </div > : <></>
-      }
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
--- src/redux/reducers/auth/authSlice.ts
+++ src/redux/reducers/auth/authSlice.ts
@@ -1,62 +1,57 @@
-import { TPermission, TRole, TUser } from "@/interface/auth.interface";
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {TPermission, TUser} from "@/interface/auth.interface";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 type TSetUserData = {
-    user: TUser,
-    permissions: TPermission[]
-}
+  user: TUser;
+  permissions: TPermission[];
+};
 
 type TInitialState = {
-    isAuthenticated: boolean,
-    role: TRole | null,
-    user: TUser | null,
-    permissions: TPermission[],
-    isVerified: boolean,
-    token: string | null,
-    resetSentTime?: string | null,
-    verificationSentTime?: string | null
-}
+  isAuthenticated: boolean;
+  user: TUser | null;
+  permissions: TPermission[];
+  isVerified: boolean;
+  token: string | null;
+  resetSentTime?: string | null;
+  verificationSentTime?: string | null;
+};
 
 const initialState: TInitialState = {
-    isAuthenticated: false,
-    role: null,
-    user: null,
-    permissions: [],
-    isVerified: false,
-    token: null,
-    resetSentTime: null,
-    verificationSentTime: null
-}
-
-
+  isAuthenticated: false,
+  user: null,
+  permissions: [],
+  isVerified: false,
+  token: null,
+  resetSentTime: null,
+  verificationSentTime: null,
+};
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState: initialState,
-    reducers: {
-        updateAuthData: (state, action: PayloadAction<TInitialState>) => {
-            state.isAuthenticated = action.payload.isAuthenticated;
-            state.role = action.payload.role;
-            state.user = action.payload.user;
-            state.permissions = action.payload.permissions;
-            state.isVerified = action.payload.isVerified;
-            state.token = action.payload.token;
-        },
-        resetAuthData: () => initialState,
-        setUserData: (state, action: PayloadAction<TSetUserData>) => {
-            state.user = action.payload.user
-            state.permissions = action.payload.permissions
-        },
-        setResetSentTime: (state, action: PayloadAction<string | null>) => {
-            state.resetSentTime = action.payload
-        },
+  name: "auth",
+  initialState: initialState,
+  reducers: {
+    updateAuthData: (state, action: PayloadAction<TInitialState>) => {
+      state.isAuthenticated = action.payload.isAuthenticated;
+      state.user = action.payload.user;
+      state.permissions = action.payload.permissions;
+      state.isVerified = action.payload.isVerified;
+      state.token = action.payload.token;
+    },
+    resetAuthData: () => initialState,
+    setUserData: (state, action: PayloadAction<TSetUserData>) => {
+      state.user = action.payload.user;
+      state.permissions = action.payload.permissions;
+    },
+    setResetSentTime: (state, action: PayloadAction<string | null>) => {
+      state.resetSentTime = action.payload;
+    },
 
-        setVerificationSentTime: (state, action: PayloadAction<string | null>) => {
-            state.verificationSentTime = action.payload
-        }
-    }
-})
+    setVerificationSentTime: (state, action: PayloadAction<string | null>) => {
+      state.verificationSentTime = action.payload;
+    },
+  },
+});
 
-export const { updateAuthData, resetAuthData, setUserData, setResetSentTime, setVerificationSentTime } = authSlice.actions
+export const {updateAuthData, resetAuthData, setUserData, setResetSentTime, setVerificationSentTime} = authSlice.actions;
 
-export default authSlice.reducer
+export default authSlice.reducer;
