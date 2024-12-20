2521d04c31355c73f05894defc37f5c3a25e42d3
MHKhan994 <khanmahmud994@gmail.com>
2024-12-20 17:28:31 +0600

    bulk upload success table

--- src/components/product/uploadResults/SuccessResultTable.tsx
+++ src/components/product/uploadResults/SuccessResultTable.tsx
@@ -1,22 +1,23 @@
 import { DataTable } from '@/components/custom/DataTable';
-import { Button } from '@/components/ui/button';
-import Select from '@/components/ui/select';
 import { TSuccessData } from '@/interface/bulkupload.interface';
 import { ColumnDef } from '@tanstack/react-table';
 import React, { useEffect, useState } from 'react';
-import { FaAngleLeft, FaAngleRight } from 'react-icons/fa6';
 
 const SuccessResultTable = ({
   successData,
 }: {
   successData: TSuccessData[];
 }) => {
+  // eslint-disable-next-line no-unused-vars
   const [page, setPage] = useState(1);
+  // eslint-disable-next-line no-unused-vars
   const [limit, setLimit] = useState(20);
   const [totalPages, setTotalPages] = useState(
     Math.ceil(successData.length / limit),
   );
   const [data, setData] = useState<TSuccessData[]>([]);
+
+  console.log(setPage, setLimit);
 
   useEffect(() => {
     if (page < totalPages) {
@@ -47,69 +48,10 @@ const SuccessResultTable = ({
     },
   ];
 
-  const handlePrev = () => {
-    if (page >= 1) {
-      return;
-    }
-    setPage((prev) => prev - 1);
-  };
-
-  const handleNext = () => {};
   return (
     <>
       <div className="bg-lavender-mist">
         <DataTable columns={columns} data={data} />
-      </div>
-      <div className="flex items-center justify-between">
-        <div className="text-base">
-          {limit * page} out of {successData.length}
-        </div>
-        <div className="flex">
-          <Button variant={'plain'} onClick={handlePrev}>
-            <FaAngleLeft /> Prev
-          </Button>
-          {Array.from({ length: totalPages })
-            .slice(page, page + 5)
-            .map((item, i: number) => {
-              // if (i > page + 2 || i < page - 2) {
-              //     return <Button key={i}>{i}</Button>
-              // }
-              return (
-                <Button
-                  onClick={() => setPage(i)}
-                  variant={page === i ? 'default' : 'secondary'}
-                  key={i}
-                >
-                  {i}
-                </Button>
-              );
-            })}
-          <Button variant={'plain'} onClick={handleNext}>
-            {' '}
-            Next <FaAngleRight />
-          </Button>
-        </div>
-
-        <div>
-          <Select
-            data={[
-              {
-                label: '10 / page',
-                value: 10,
-              },
-              {
-                label: '20 / page',
-                value: 20,
-              },
-              {
-                label: '50 / page',
-                value: 50,
-              },
-            ]}
-            value={limit}
-            onChange={(val) => setLimit(val as number)}
-          />
-        </div>
       </div>
     </>
   );
